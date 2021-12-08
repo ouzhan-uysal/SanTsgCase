bf556f7cc4c3ae314f7371901e94e398aa5560b9
Oğuzhan <ouzhan.ysl@gmail.com>
2021-12-08 03:25:47 +0300

    Add View Component
    Flight & Hotel Service

---
+++ SanTsgCase/ViewComponents/FlightList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SanTsgCase.ViewComponents
+{
+    public class FlightList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ SanTsgCase/ViewComponents/HotelList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SanTsgCase.ViewComponents
+{
+    public class HotelList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

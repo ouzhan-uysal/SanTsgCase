a6479ae7ae51780655404eaf014fed48f6b9a619
Oğuzhan <ouzhan.ysl@gmail.com>
2021-12-08 03:28:20 +0300

    Update Controllers
    Flight C. & Hotel C. ların HttpPost işlevleri eklendi.

--- SanTsgCase/Controllers/FlightController.cs
+++ SanTsgCase/Controllers/FlightController.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SanTsgCase.Controllers
 {
     public class FlightController : Controller
     {
+        ServiceManager fr = new ServiceManager(new EFFlightRepository());
+
+        [HttpGet]
         public IActionResult Index()
         {
+            var values = fr.GetFlightList();
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Flight p)
+        {
+            FlightValidator fv = new FlightValidator();
+            ValidationResult results = fv.Validate(p);
+            if (results.IsValid)
+            {
+                fr.AddFlight(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
--- SanTsgCase/Controllers/HotelController.cs
+++ SanTsgCase/Controllers/HotelController.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SanTsgCase.Controllers
 {
     public class HotelController : Controller
     {
+        ServiceManager hr = new ServiceManager(new EFHotelRepository());
+
+        [HttpGet]
         public IActionResult Index()
         {
+            var values = hr.GetHotelList();
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Hotel p)
+        {
+            HotelValidator hv = new HotelValidator();
+            ValidationResult results = hv.Validate(p);
+            if (results.IsValid)
+            {
+                hr.AddHotel(p);
+                return RedirectToAction("Index", "Hotel");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
